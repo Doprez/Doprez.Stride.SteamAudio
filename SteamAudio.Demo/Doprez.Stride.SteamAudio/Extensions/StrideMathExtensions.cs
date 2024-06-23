15e3af44076f884987c06fe280a401127ea3e1d3
Doprez <73259914+Doprez@users.noreply.github.com>
2024-06-23 01:24:56 -0600

    basic requirements

---
+++ SteamAudio.Demo/Doprez.Stride.SteamAudio/Extensions/StrideMathExtensions.cs
@@ -0,0 +1,15 @@
+﻿using NVector3 = System.Numerics.Vector3;
+using System.Runtime.CompilerServices;
+using Stride.Core.Mathematics;
+using SteamAudio;
+
+namespace Doprez.Stride.SteamAudio;
+/// <summary>
+/// Generic extensions for Stride.Core.Mathematics types.
+/// </summary>
+public static class IPLMathExtensions
+{
+	public static IPL.Vector3 ToIPL(this Vector3 v) => Unsafe.As<Vector3, IPL.Vector3>(ref v);
+
+	public static IPL.Vector3 ToIPL(this NVector3 v) => Unsafe.As<NVector3, IPL.Vector3>(ref v);
+}
---
+++ SteamAudio.Demo/Doprez.Stride.SteamAudio/SteamAudio/SteamAudioListener.cs
@@ -0,0 +1,14 @@
+﻿using Doprez.Stride.SteamAudio.Processors;
+using Stride.Core;
+using Stride.Engine;
+using Stride.Engine.Design;
+
+namespace Doprez.Stride.SteamAudio;
+[DataContract]
+[Display("Steam Audio Listener")]
+[ComponentCategory("Audio")]
+[DefaultEntityComponentProcessor(typeof(SteamAudioListenerProcessor), ExecutionMode = ExecutionMode.Runtime)]
+public class SteamAudioListener : EntityComponent
+{
+	
+}
